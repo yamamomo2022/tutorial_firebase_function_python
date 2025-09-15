df4f8e5239010c944084fedd1ab041639ba88044
viole <violetraisondetre@gmail.com>
2025-09-15 19:02:16 +0900

    fet:basic agent

---
+++ functions/agent.py
@@ -0,0 +1,44 @@
+from vertexai import agent_engines, init
+from langchain_google_vertexai import HarmBlockThreshold, HarmCategory
+
+init(project="ai-agent-hackathon-3-471422", location="us-central1")
+
+safety_settings = {
+    HarmCategory.HARM_CATEGORY_UNSPECIFIED: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+}
+
+model_kwargs = {
+    # temperature (float): The sampling temperature controls the degree of
+    # randomness in token selection.
+    "temperature": 0.28,
+    # max_output_tokens (int): The token limit determines the maximum amount of
+    # text output from one prompt.
+    "max_output_tokens": 1000,
+    # top_p (float): Tokens are selected from most probable to least until
+    # the sum of their probabilities equals the top-p value.
+    "top_p": 0.95,
+    # top_k (int): The next token is selected from among the top-k most
+    # probable tokens. This is not supported by all model versions. See
+    # https://cloud.google.com/vertex-ai/generative-ai/docs/multimodal/image-understanding#valid_parameter_values
+    # for details.
+    "top_k": None,
+    # safety_settings (Dict[HarmCategory, HarmBlockThreshold]): The safety
+    # settings to use for generating content.
+    # (you must create your safety settings using the previous step first).
+    "safety_settings": safety_settings,
+}
+
+model = "gemini-2.0-flash"
+
+agent = agent_engines.LangchainAgent(
+    model=model,                # Required.
+    model_kwargs=model_kwargs,  # Optional.
+)
+
+if __name__ == "__main__":
+    response = agent.query(input="What is the exchange rate from US dollars to SEK today?")
+    print(response)
